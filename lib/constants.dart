36080eaa1a4b4c809232e1f2e8128a67d7b250c5
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-09-14 23:04:25 +0530

    initial setup

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+// Colors that we use in our app
+final kPrimaryColor = Color(0xFF0C9869);
+final kTextColor = Color(0xFF3C4046);
+final kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      
+    );
+  }
+}
