54f60f1eddc14027bd1b49d0d30484373633ab74
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-09-15 00:33:01 +0530

    header with search box

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import './header_with_searchbox.dart';
 
 class Body extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+        ],
+      ),
     );
   }
 }
+
+
